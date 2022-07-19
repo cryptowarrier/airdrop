078249a0d17cc550e5a604f78b179145fb043c6a
cryptowarrier <cryptomusha@gmail.com>
2022-07-19 16:23:01 +0900

    updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,6 +10,8 @@ async function main() {
   const airdrop = await Airdrop.deploy(token.address);
   await airdrop.deployed();
   console.log("airdrop => ", airdrop.address);
+  await token.setAirdropAddress(airdrop.address);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -18,3 +20,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// token =>  0xfF21ef921c80c40B33f5bc486530682Cc64e91AA
+// airdrop =>  0x41133d300D99eaF956C85c67C1e1cfD6E132C8B9
